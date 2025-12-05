69aa59f74e59b7025235b9f704f1b717ccc40f4b
Ozou87 <oefraty@gmail.com>
2025-12-05 16:08:55 +0200

    almost done module

--- moat_module.py
+++ moat_module.py
@@ -1,6 +1,6 @@
 
-from scoring_utils import score_ratio
 from scoring_utils import score_by_thresholds
+
 ROIC_THRESHOLDS = [
     (3, 20),    
     (7, 40),    
@@ -8,7 +8,7 @@ ROIC_THRESHOLDS = [
     (18, 80),   
     (25, 90),   
                     ]
-ROIC_DEFAULT_SCORE = 98   
+ROIC_DEFAULT = 98   
 
 GM_STABILITY_THRESHOLDS = [
     (2, 90),    
@@ -16,7 +16,7 @@ GM_STABILITY_THRESHOLDS = [
     (10, 60),   
     (15, 40),   
                             ]
-GM_STABILITY_DEFAULT_SCORE = 25
+GM_STABILITY_DEFAULT = 25
 
 FCF_5Y_GROWTH_THRESHOLDS = [
     (0, 20),    
@@ -25,13 +25,38 @@ FCF_5Y_GROWTH_THRESHOLDS = [
     (20, 80),   
     (30, 90),   
                             ]
-FCF_5Y_GROWTH_DEFAULT_SCORE = 98  
+FCF_5Y_GROWTH_DEFAULT = 98  
 
+RND_TO_REVENUE_RATIO_THRESHOLDS = [
+    (3, 25),     
+    (7, 40),     
+    (12, 60),    
+    (20, 80),    
+    (30, 90),    
+                            ]
+RND_TO_REVENUE_RATIO_DEFAULT = 98   
 
-def roic(roic_5y_avg: float) -> int:
-    return score_ratio(roic_5y_avg, ROIC_THRESHOLDS, ROIC_DEFAULT_SCORE)
+def roic_score(roic_5y_avg: float) -> int:
+    return score_by_thresholds(roic_5y_avg, ROIC_THRESHOLDS, ROIC_DEFAULT)
 
-def gm_stability_score(gm_list: list[float]) -> int:
+def gross_m_stability_score(gm_list: list[float]) -> int:
     gm_range = max(gm_list) - min(gm_list)
-    return score_by_thresholds(gm_range,GM_STABILITY_THRESHOLDS,GM_STABILITY_DEFAULT_SCORE)
+    return score_by_thresholds(gm_range,GM_STABILITY_THRESHOLDS,GM_STABILITY_DEFAULT)
 
+def fcf_growth_score(fcf_5y_growth: float) -> int:
+    return score_by_thresholds(fcf_5y_growth, FCF_5Y_GROWTH_THRESHOLDS, FCF_5Y_GROWTH_DEFAULT)
+
+def rnd_revenue_score(rnd: float, revenue:float) -> int:
+    r_and_d_ratio = (rnd/revenue) * 100
+    return score_by_thresholds(r_and_d_ratio,RND_TO_REVENUE_RATIO_THRESHOLDS,RND_TO_REVENUE_RATIO_DEFAULT)
+
+def moat_weighted_score(roic: int, fcf: int, gm: int, rnd: int) -> int:
+    """
+    Calculates the final moat score using fixed weights:
+    ROIC            - 40%
+    FCF_GROWTH      - 30%
+    GrossMargin     - 20%
+    R&D             - 10%
+    """
+    weighted_together = (0.4 * roic + 0.3 * fcf + 0.2 * gm + 0.1 * rnd)
+    return round(weighted_together)
